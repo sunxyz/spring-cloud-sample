49c8f135ba7ce37106d713b3d724163141de8e0d
yangrd <2445213759@qq.com>
2019-09-28 16:24:03 +0800

    feature:  breaker-dashboard 增加默认地址

--- breaker-dashboard/src/main/java/com/yangrd/springcloud/example/turbine/BreakerDashboardApplication.java
+++ breaker-dashboard/src/main/java/com/yangrd/springcloud/example/turbine/BreakerDashboardApplication.java
@@ -1,18 +1,31 @@
 package com.yangrd.springcloud.example.turbine;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * @author yangrd
  * @date 2019/09/28
  */
+@Controller
 @EnableHystrixDashboard
 @EnableTurbine
 @SpringCloudApplication
 public class BreakerDashboardApplication {
+
+
+    @Value("${turbineUrl}")
+    private String turbineUrl;
+
+    @GetMapping
+    public String index(){
+        return "redirect:"+turbineUrl;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BreakerDashboardApplication.class, args);
